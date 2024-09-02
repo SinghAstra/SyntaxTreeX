9832a3f4edb9ab216e40747d58b46710da0b4be7
Astra <abhaypratapsinghwd@gmail.com>
2024-09-02 13:19:46 +0530

    Chat App frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="chat-box-container">
+      <div className="chat-box"></div>
+      <input type="text" placeholder="Type Your Message here...." />
+      <input type="text" placeholder="Room Number" />
+    </div>
+  );
+}
+
+export default App;
